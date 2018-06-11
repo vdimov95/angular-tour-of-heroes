3c678d3ef8d64fa749d09c52e2ba79fd674a9736
Vladimir Dimov <v.dimov@blubito.com>
2018-06-11 16:24:09 +0300

    Generated new component Dashboard, created routing for Heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,23 +8,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  hero: Hero = {
-    id:1,
-    name:'Windstorm',
-  };
-
-  constructor() { }
+   
+  selectedHero: Hero;
+ 
+  heroes: Hero[];
+ 
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
-
-}
-
-
